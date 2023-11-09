770732772272390b03f9909c1de4161fcafd8d58
ayoitsjango <muradmalik252@gmail.com>
2023-11-09 16:20:29 -0500

    names updates

--- public/script.js
+++ public/script.js
@@ -1,17 +1,17 @@
 document.addEventListener("click", (e) => {
-  const isDropdownButton = e.target.matches("[data-dropdown-button]");
-  const dropdown = e.target.closest("[data-dropdown]");
+const isDropdownButton = e.target.matches("[data-dropdown-button]");
+const dropdown = e.target.closest("[data-dropdown]");
 
-  if (isDropdownButton) {
-    if (dropdown) {
-      dropdown.classList.toggle("active");
-    }
-  } else if (!e.target.closest(".dropdown-menu")) {
-    const activeDropdowns = document.querySelectorAll(".dropdown.active");
-    activeDropdowns.forEach((activeDropdown) => {
-      if (activeDropdown !== dropdown) {
-        activeDropdown.classList.remove("active");
-      }
-    });
+if (isDropdownButton) {
+  if (dropdown) {
+    dropdown.classList.toggle("active");
   }
+} else if (!e.target.closest(".dropdown-menu")) {
+  const activeDropdowns = document.querySelectorAll(".dropdown.active");
+  activeDropdowns.forEach((activeDropdown) => {
+    if (activeDropdown !== dropdown) {
+      activeDropdown.classList.remove("active");
+    }
+  });
+}
 });
