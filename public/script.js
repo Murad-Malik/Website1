c9d3fa4d719536db53ae663da58ce529bd1040d8
ayoitsjango <muradmalik252@gmail.com>
2023-11-09 21:59:58 -0500

    added footer to services page

--- public/script.js
+++ public/script.js
@@ -1,30 +1,41 @@
+console.log("Script.js loaded");
+
 document.addEventListener("DOMContentLoaded", () => {
   const servicesButton = document.querySelector("[data-dropdown-button='Services']");
-  const dropdownMenu = servicesButton.nextElementSibling;
 
-  // Show the dropdown menu on hover
-  servicesButton.addEventListener("mouseenter", () => {
-    dropdownMenu.style.opacity = "1";
-    dropdownMenu.style.pointerEvents = "auto";
-    dropdownMenu.style.transform = "translateY(0)";
-  });
+  if (servicesButton) {
+    const dropdownMenu = servicesButton.nextElementSibling;
 
-  // Hide the dropdown menu when the mouse leaves the button or the dropdown
-  servicesButton.addEventListener("mouseleave", () => {
-    dropdownMenu.style.opacity = "0";
-    dropdownMenu.style.pointerEvents = "none";
-    dropdownMenu.style.transform = "translateY(-10px)";
-  });
+    // Show the dropdown menu on hover
+    servicesButton.addEventListener("mouseenter", () => {
+      if (dropdownMenu) {
+        dropdownMenu.style.opacity = "1";
+        dropdownMenu.style.pointerEvents = "auto";
+        dropdownMenu.style.transform = "translateY(0)";
+      }
+    });
 
-  dropdownMenu.addEventListener("mouseenter", () => {
-    dropdownMenu.style.opacity = "1";
-    dropdownMenu.style.pointerEvents = "auto";
-    dropdownMenu.style.transform = "translateY(0)";
-  });
+    // Hide the dropdown menu when the mouse leaves the button or the dropdown
+    servicesButton.addEventListener("mouseleave", () => {
+      if (dropdownMenu) {
+        dropdownMenu.style.opacity = "0";
+        dropdownMenu.style.pointerEvents = "none";
+        dropdownMenu.style.transform = "translateY(-10px)";
+      }
+    });
 
-  dropdownMenu.addEventListener("mouseleave", () => {
-    dropdownMenu.style.opacity = "0";
-    dropdownMenu.style.pointerEvents = "none";
-    dropdownMenu.style.transform = "translateY(-10px)";
-  });
+    if (dropdownMenu) {
+      dropdownMenu.addEventListener("mouseenter", () => {
+        dropdownMenu.style.opacity = "1";
+        dropdownMenu.style.pointerEvents = "auto";
+        dropdownMenu.style.transform = "translateY(0)";
+      });
+
+      dropdownMenu.addEventListener("mouseleave", () => {
+        dropdownMenu.style.opacity = "0";
+        dropdownMenu.style.pointerEvents = "none";
+        dropdownMenu.style.transform = "translateY(-10px)";
+      });
+    }
+  }
 });
