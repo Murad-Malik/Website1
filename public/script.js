44205a711394a856e3ad4813fb4d887b2975d6f2
ayoitsjango <muradmalik252@gmail.com>
2023-11-09 19:37:00 -0500

    hover to active dropdown menu

--- public/script.js
+++ public/script.js
@@ -1,17 +1,30 @@
-document.addEventListener("click", (e) => {
-const isDropdownButton = e.target.matches("[data-dropdown-button]");
-const dropdown = e.target.closest("[data-dropdown]");
+document.addEventListener("DOMContentLoaded", () => {
+  const servicesButton = document.querySelector("[data-dropdown-button='Services']");
+  const dropdownMenu = servicesButton.nextElementSibling;
 
-if (isDropdownButton) {
-  if (dropdown) {
-    dropdown.classList.toggle("active");
-  }
-} else if (!e.target.closest(".dropdown-menu")) {
-  const activeDropdowns = document.querySelectorAll(".dropdown.active");
-  activeDropdowns.forEach((activeDropdown) => {
-    if (activeDropdown !== dropdown) {
-      activeDropdown.classList.remove("active");
-    }
+  // Show the dropdown menu on hover
+  servicesButton.addEventListener("mouseenter", () => {
+    dropdownMenu.style.opacity = "1";
+    dropdownMenu.style.pointerEvents = "auto";
+    dropdownMenu.style.transform = "translateY(0)";
   });
-}
+
+  // Hide the dropdown menu when the mouse leaves the button or the dropdown
+  servicesButton.addEventListener("mouseleave", () => {
+    dropdownMenu.style.opacity = "0";
+    dropdownMenu.style.pointerEvents = "none";
+    dropdownMenu.style.transform = "translateY(-10px)";
+  });
+
+  dropdownMenu.addEventListener("mouseenter", () => {
+    dropdownMenu.style.opacity = "1";
+    dropdownMenu.style.pointerEvents = "auto";
+    dropdownMenu.style.transform = "translateY(0)";
+  });
+
+  dropdownMenu.addEventListener("mouseleave", () => {
+    dropdownMenu.style.opacity = "0";
+    dropdownMenu.style.pointerEvents = "none";
+    dropdownMenu.style.transform = "translateY(-10px)";
+  });
 });
